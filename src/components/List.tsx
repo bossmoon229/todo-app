5af84b93c5f35500328b10f6da96ea1fd12a96c2
bossmoon229 <alinaignatich@gmail.com>
2025-03-18 23:38:41 +0200

    create two-way binding with checkbox

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,23 +4,23 @@ import { todos } from "../data/data";
 export default function List() {
   const [checked, setChecked] = useState(todos);
 
-  const handleCheckTodos = (id: number) =>{
-    console.log('====================================');
-    console.log('checked');
-    console.log('====================================');
-    setChecked(checked.map((todo)=>
-    todo.id === id ? {...todo, completed: !todo.completed} : todo
-    ))
-  }
+  const handleCheckTodos = (id: number) => {
+    setChecked((prevState) =>
+      prevState.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
 
   return (
     <div className="list-wrapper">
       <div className="todos">
         <h1>Things to do</h1>
         <ul>
-          {todos.map((todo) => (
+          {checked.map((todo) => (
             <li key={todo.id}>
-              <input type='checkbox'
+              <input
+                type="checkbox"
                 checked={todo.completed}
                 onChange={() => handleCheckTodos(todo.id)}
               />
