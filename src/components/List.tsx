89c36328d60228d2a8b91f033b0fc7d4775b9292
bossmoon229 <alinaignatich@gmail.com>
2025-03-20 13:25:44 +0200

    add functional for task and buttons

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { todos } from "../data/data";
+import trash from "../../public/trash-svgrepo-com.svg";
 
 export default function List() {
   const [checked, setChecked] = useState(todos);
+  const [open, setOpen] = useState(false);
+  const [newTask, setNewTask] = useState("");
 
   const handleCheckTodos = (id: number) => {
     setChecked((prevState) =>
@@ -12,22 +15,63 @@ export default function List() {
     );
   };
 
+  const handleDelete = (id: number) => {
+    setChecked((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleInput = () => {
+    setOpen(!open);
+  };
+
+  const handleAddTask = () => {
+    if (newTask.trim() === "") return;
+    const newTodo = {
+      id: checked.length + 1,
+      text: newTask,
+      completed: false,
+    };
+    
+    setChecked((prev) => [...prev, newTodo]);
+    setNewTask("");
+    setOpen(false);
+    console.log(todos);
+    
+  };
+
   return (
     <div className="list-wrapper">
       <div className="todos">
         <h1>Things to do</h1>
         <ul>
           {checked.map((todo) => (
-            <li key={todo.id}>
+            <li key={todo.id} className={todo.completed ? "completed" : ""}>
               <input
                 type="checkbox"
                 checked={todo.completed}
                 onChange={() => handleCheckTodos(todo.id)}
               />
               {todo.text}
+              <img
+                className="trash-icon"
+                onClick={() => handleDelete(todo.id)}
+                src={trash}
+                alt=""
+              />
             </li>
           ))}
         </ul>
+        {open && (
+          <>
+            <input
+              type="text"
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+              placeholder="Enter your task"
+            />
+            <button onClick={handleAddTask}>Save</button>
+          </>
+        )}
+        <button onClick={toggleInput}>{open ? "Cancel" : "Add task"}</button>
       </div>
     </div>
   );
